79fb0b3331329c523bf56d24f21269456d6c778f
deekep <MVSein@stud.kantiana.ru>
2022-02-02 22:29:01 +0200

    working version

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+bool check_palindrome(int i, std::string str){
+    int len = str.length();
+    if(i >= len/2) return false;
+
+    if(str[i] == str[len-i-1]){
+        check_palindrome(i + 1,str);
+    } else {
+        return false;
+    }
+    return true;
+}
+
+int main(){
+    std::string str;
+    std::cout << "Введите слово: ";
+    std::cin >> str;
+    if(check_palindrome(0,str)){
+        std::cout << "Слово является палиндромом";
+    }
+    else{
+        std::cout << "Слово не является палиндромом";
+    }
     return 0;
 }
